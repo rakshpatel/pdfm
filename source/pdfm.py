9eb9a50f4f1f78d7893cdb234b1cbaf3f0daacdf
Rakhit Patel <raksh.patel@gmail.com>
2021-10-10 16:03:53 +0530

    feat: Adds code for pdf split
    Adds code for
    > PDF Split logic
    > Requirements.txt

--- source/pdfm.py
+++ source/pdfm.py
@@ -1,8 +1,10 @@
 """
 Simple utility to work with PDF
 """
+import os
 import argparse
 import sys
+import PyPDF2
 
 def get_arguments():
     """
@@ -43,18 +45,19 @@ def verify_arguments(args):
     Arguments:
         args: received arguments.
     Returns:
-        None.
+        True/False based on validations.
     """
 
-    if args.action == "split" and not args.parts:
-        print("Value for parts is mandatory when action is split")
+    if args.action == "split" and (not args.parts or not args.files):
+        print("Value for parts and files is mandatory when action is split")
         usage()
-        sys.exit(1)
+        return False
 
     if args.action == "merge" and not args.files:
         print("List of files is mandatory when action is merge")
         usage()
-        sys.exit(1)
+        return False
+    return True
 
 def usage():
     """
@@ -64,10 +67,52 @@ def usage():
     Returns:
         None.
     """
+    print("*" * 80)
     print("Utility to either merge the given files, or split files into multiple PDFs")
     print("pdfm.py -a split -p parts or pdfm.py --action split --parts parts")
     print("pdfm.py -a merge -f file1,file2 or pdfm.py --action split --files file1,file2")
+    print("*" * 80)
 
+def pdf_split(file, parts):
+    """
+    Description: Split the given pdf file into multiple files
+    Arguments:
+        file: File to be splitted.
+        parts: No of parts. File should be split into parts.
+    Returns:
+        None.
+    """
+    file_path, file_name = os.path.split(file)
+    output_file_path = file_path
+    output_file_name = os.path.join(output_file_path, file_name.split(".pdf")[0])
+    
+    with open(file, 'rb') as file_object:
+        reader = PyPDF2.PdfFileReader(file_object)
+        writer = PyPDF2.PdfFileWriter()
+        num_pages = reader.numPages
+        page_per_pdf = round(num_pages/parts)
+        file_part = 0
+        written = False
+        print("Each part will contain, approximately {} pages.\nLast file may contain more/less based on total pages in original file.".format(page_per_pdf))
+        if parts > num_pages:
+            print("File can't be split into {} parts, as it has only {} pages. Parts should be <= {}".format(parts, num_pages, num_pages))
+            sys.exit(1)
+        
+        for page in range(num_pages):
+            written = False
+            writer.addPage(reader.getPage(page))
+            if (page + 1) % page_per_pdf == 0:
+                with open("{}_{}.pdf".format(output_file_name, file_part), "wb") as file:
+                    writer.write(file)
+                file_part += 1
+                writer = PyPDF2.PdfFileWriter()
+                written = True
+        
+        if not written:
+            with open("{}_{}.pdf".format(output_file_name, file_part), "wb") as file:
+                writer.write(file)
+            file_part += 1
+            writer = PyPDF2.PdfFileWriter()
 
 def main():
     """
@@ -78,7 +123,9 @@ def main():
         None.
     """
     args = get_arguments()
-    verify_arguments(args)
+    if verify_arguments(args):
+        if args.action == "split":
+            pdf_split(args.files, args.parts)
 
 if __name__ == '__main__':
     main()
